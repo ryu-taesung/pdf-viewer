ac8784bedfc37479b4499d9e59421d4d2820dec9
Ryu, Taesung <ryu.taesung@gmail.com>
2024-07-25 10:45:42 -0400

    add version tag query to make pyinstaller

--- make_pyinstaller.sh
+++ make_pyinstaller.sh
@@ -3,4 +3,5 @@
 ############################# make_pyinstaller.sh ##############################
 ################################################################################
 
+python3 get_git_version_tag.py
 pyinstaller --windowed --add-data="file-pdf.png:." --add-data="version.txt:." --icon="file-pdf.png" --noconfirm pdf_viewer.py
